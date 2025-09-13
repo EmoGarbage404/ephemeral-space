a0f15de41ceaffe91175f955e01e4dc74820be95
EmoGarbage404 <retron404@gmail.com>
2025-09-13 17:19:49 -0400

    Add pallet good counting functionality

---
+++ Content.Shared/_ES/Cargo/Storeroom/Components/ESStoreroomStationComponent.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._ES.Cargo.Storeroom.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class ESStoreroomStationComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public List<ESStoreroomContainerEntry> Containers;
+}
