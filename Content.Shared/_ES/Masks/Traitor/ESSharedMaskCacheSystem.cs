3a24ff0461a8f7c88cc3843cb948070eca7418d4
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2025-10-04 11:06:16 -0400

    Traitor caches and Marauder Mask (#182)
    * Add marauder prototype & basic traitor cache system
    
    * Start moving cache over to spawn-on-demand model
    
    * Finish ceiling cache implementation
    
    * Add cache location indicator

---
+++ Content.Shared/_ES/Masks/Traitor/ESSharedMaskCacheSystem.cs
@@ -0,0 +1,98 @@
+using Content.Shared._ES.Masks.Traitor.Components;
+using Content.Shared.Alert;
+using Content.Shared.DoAfter;
+using Content.Shared.Mind;
+using Content.Shared.Popups;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Physics.Events;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._ES.Masks.Traitor;
+
+public abstract class ESSharedMaskCacheSystem : EntitySystem
+{
+    [Dependency] private readonly AlertsSystem _alerts = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
+    [Dependency] private readonly SharedMindSystem _mind = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] protected readonly SharedTransformSystem TransformSystem = default!;
+
+    protected static readonly EntProtoId<ESCeilingCacheComponent> CeilingCachePrototype = "ESMarkerTraitorCeilingCache";
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<ESCeilingCacheComponent, StartCollideEvent>(OnStartCollide);
+        SubscribeLocalEvent<ESCeilingCacheComponent, EndCollideEvent>(OnEndCollide);
+        SubscribeLocalEvent<ESCeilingCacheComponent, ESRevealCacheDoAfterEvent>(OnRevealCacheDoAfter);
+
+        SubscribeLocalEvent<ESCeilingCacheContactingComponent, ESRevealCacheAlertEvent>(OnRevealCacheAlert);
+    }
+
+    private void OnStartCollide(Entity<ESCeilingCacheComponent> ent, ref StartCollideEvent args)
+    {
+        if (!_mind.TryGetMind(args.OtherEntity, out var mindUid, out _) ||
+            mindUid != ent.Comp.MindId)
+            return;
+        _alerts.ShowAlert(args.OtherEntity, ent.Comp.CacheAlertProto);
+        var comp = EnsureComp<ESCeilingCacheContactingComponent>(args.OtherEntity);
+        comp.Cache = ent;
+    }
+
+    private void OnEndCollide(Entity<ESCeilingCacheComponent> ent, ref EndCollideEvent args)
+    {
+        if (!_mind.TryGetMind(args.OtherEntity, out var mindUid, out _) ||
+            mindUid != ent.Comp.MindId)
+            return;
+        _alerts.ClearAlert(args.OtherEntity, ent.Comp.CacheAlertProto);
+        RemComp<ESCeilingCacheContactingComponent>(args.OtherEntity);
+    }
+
+    private void OnRevealCacheAlert(Entity<ESCeilingCacheContactingComponent> ent, ref ESRevealCacheAlertEvent args)
+    {
+        if (ent.Comp.DoAfterKey is not null)
+            return;
+
+        if (TerminatingOrDeleted(ent.Comp.Cache))
+        {
+            RemCompDeferred(ent, ent.Comp);
+            return;
+        }
+
+        var ev = new ESRevealCacheDoAfterEvent();
+        _doAfter.TryStartDoAfter(new DoAfterArgs(EntityManager,
+            ent.Owner,
+            TimeSpan.FromSeconds(3),
+            ev,
+            ent.Comp.Cache, // TODO: maybe target something else?
+            ent.Comp.Cache,
+            ent.Owner
+            )
+            {
+                BreakOnMove = true,
+                BlockDuplicate = true,
+                DuplicateCondition = DuplicateConditions.SameTarget,
+            });
+    }
+
+    private void OnRevealCacheDoAfter(Entity<ESCeilingCacheComponent> ent, ref ESRevealCacheDoAfterEvent args)
+    {
+        if (args.Cancelled)
+            return;
+
+        var pos = TransformSystem.GetMapCoordinates(ent);
+        var cache = PredictedSpawnAtPosition(ent.Comp.CacheLoot, TransformSystem.ToCoordinates(pos));
+        PredictedQueueDel(ent);
+        _popup.PopupPredicted(Loc.GetString("es-ceiling-cache-popup"), cache, args.User);
+        _audio.PlayPredicted(ent.Comp.RevealSound, cache, args.User);
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed partial class ESRevealCacheDoAfterEvent : DoAfterEvent
+{
+    public override DoAfterEvent Clone() => this;
+}
+
