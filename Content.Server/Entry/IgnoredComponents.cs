16b0613316bf8d0adbf24469b864f54e12f7e527
EmoGarbage404 <retron404@gmail.com>
2025-11-13 01:48:37 -0500

    Add pointlight despawning visuals for sparks

--- Content.Server/Entry/IgnoredComponents.cs
+++ Content.Server/Entry/IgnoredComponents.cs
@@ -14,6 +14,7 @@ namespace Content.Server.Entry
             // ES START
             "ESInherentLight",
             "ESGenericPointLightVisualizer",
+            "ESTimedDespawnLightFade",
             "ESPoster",
             // ES END
             "ConstructionGhost",
