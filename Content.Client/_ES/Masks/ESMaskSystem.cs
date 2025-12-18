9d20e6fb423eeb150e56f771f47ccd117aff6864
EmoGarbage404 <retron404@gmail.com>
2025-12-18 13:53:59 -0500

    Fix up IC troupe icon system

--- Content.Client/_ES/Masks/ESMaskSystem.cs
+++ Content.Client/_ES/Masks/ESMaskSystem.cs
@@ -1,11 +1,14 @@
 using Content.Shared._ES.Masks;
 using Content.Shared._ES.Masks.Components;
 using Content.Shared.StatusIcon.Components;
+using Robust.Client.Player;
 
 namespace Content.Client._ES.Masks;
 
 public sealed class ESMaskSystem : ESSharedMaskSystem
 {
+    [Dependency] private readonly IPlayerManager _player = default!;
+
     public override void Initialize()
     {
         base.Initialize();
@@ -15,6 +18,14 @@ public sealed class ESMaskSystem : ESSharedMaskSystem
 
     private void OnGetStatusIcons(Entity<ESTroupeFactionIconComponent> ent, ref GetStatusIconsEvent args)
     {
+        if (_player.LocalEntity is not { } local)
+            return;
+
+        // The main filtering is done on the networking for ESTroupeFactionIconComponent,
+        // but this exists largely to catch edge cases where we still have
+        // the networked comp on the client even though we shouldn't have access to it.
+        if (GetTroupeOrNull(local) != ent.Comp.Troupe)
+            return;
         args.StatusIcons.Add(PrototypeManager.Index(ent.Comp.Icon));
     }
 }
