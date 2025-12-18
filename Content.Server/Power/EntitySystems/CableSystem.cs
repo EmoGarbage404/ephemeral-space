636dfa12e543f0714b294298a71cdfdab74c2273
EmoGarbage404 <retron404@gmail.com>
2025-12-18 18:30:39 -0500

    That's a terrible idea, actually

--- Content.Server/Power/EntitySystems/CableSystem.cs
+++ Content.Server/Power/EntitySystems/CableSystem.cs
@@ -8,11 +8,6 @@ using Content.Shared.Interaction;
 using Robust.Shared.Map;
 using CableCuttingFinishedEvent = Content.Shared.Tools.Systems.CableCuttingFinishedEvent;
 using SharedToolSystem = Content.Shared.Tools.Systems.SharedToolSystem;
-// ES START
-using Content.Shared._ES.TileFires;
-using Content.Shared.Electrocution;
-using Robust.Shared.Random;
-// ES END
 
 namespace Content.Server.Power.EntitySystems;
 
@@ -22,10 +17,6 @@ public sealed partial class CableSystem : EntitySystem
     [Dependency] private readonly SharedToolSystem _toolSystem = default!;
     [Dependency] private readonly StackSystem _stack = default!;
     [Dependency] private readonly ElectrocutionSystem _electrocutionSystem = default!;
-// ES START
-    [Dependency] private readonly IRobustRandom _random = default!;
-    [Dependency] private readonly ESSharedTileFireSystem _tileFire = default!;
-// ES END
 
     public override void Initialize()
     {
@@ -58,11 +49,6 @@ public sealed partial class CableSystem : EntitySystem
         var xform = Transform(uid);
         var ev = new CableAnchorStateChangedEvent(xform);
         RaiseLocalEvent(uid, ref ev);
-        // ES START
-        var electrified = CompOrNull<ElectrifiedComponent>(uid)?.Enabled ?? false;
-        if (electrified && _random.Prob(0.5f))
-            _tileFire.TryDoTileFire(uid, args.User);
-        // ES END
 
         if (_electrocutionSystem.TryDoElectrifiedAct(uid, args.User))
             return;
